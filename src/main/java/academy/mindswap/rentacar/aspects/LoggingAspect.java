1e135868024dc2232a8b4e3b226e2fdb4ef4582e
hicalonic <hicalonic@gmail.com>
2023-03-13 17:41:12 +0000

    devmain11

--- src/main/java/academy/mindswap/rentacar/aspects/LoggingAspect.java
+++ src/main/java/academy/mindswap/rentacar/aspects/LoggingAspect.java
@@ -8,19 +8,51 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+
 @Component
 @Aspect
 public class LoggingAspect {
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
-    @Before("execution(* academy.mindswap.rentacar.controller.UserController.createUser())")
+
+    /**
+    Before is just to log every action for all controllers.
+     */
+    @Before("execution(* academy.mindswap.rentacar.controller.*(..))")
     public void checkUserBefore(JoinPoint joinPoint) {
         logger.info("Before " + joinPoint.getSignature().getName() + " method call");
     }
 
-    @AfterReturning(pointcut = "execution(* academy.mindswap.rentacar.controller.*)")
-    public void checkUserAfter(JoinPoint joinPoint) {
+
+    /**
+     * Notificates everytime that a user is created.
+     * @param joinPoint
+     */
+    @AfterReturning(pointcut = "execution(* academy.mindswap.rentacar.controller.UserController.createUser.*)")
+    public void checkUserAfter(JoinPoint joinPoint, Object result) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Logs.txt"));
+        FileHandler handler = new FileHandler("Logs.txt", true);
+        handler.setLevel(Level.INFO);
+        logger.
         logger.info("Before " + joinPoint.getSignature().getName() + " method call");
+        logger.info("Response" + result);
+
+        handler
+
     }
+
+
+    @AfterThrowing(pointcut = "execution(* academy.mindswap.rentacar.controller.*(..))", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+        logger.error("Exception in " + joinPoint.getSignature().getName() + " method call");
+        logger.error("Exception: " + exception);
+    }
+
+
 
 
 }
